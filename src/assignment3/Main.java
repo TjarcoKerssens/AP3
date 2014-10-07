cc796c38fb6e3283ab555a60c51464c605fc2174
tjarco <info@tjarcokerssens.nl>
2014-10-07 16:40:25 +0200

    parser

--- src/assignment3/Main.java
+++ src/assignment3/Main.java
@@ -1,26 +1,86 @@
 package assignment3;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Arrays;
 import java.util.Scanner;
 import java.util.regex.Pattern;
 
 public class Main {
+	private boolean descending, lowercase;
 
-	void run() {
-		Scanner in = new Scanner(System.in);
-		in.useDelimiter(Pattern.compile("[^a-zA-Z0-9]+"));
-		while (in.hasNext()) {
-			String s = in.next();
-			if(Character.isLetter(s.charAt(0))){
-				System.out.println("Identifier: " + s);
-			}else{
-				System.out.println("non-Identifier: " + s);
-			}
-		}
-		in.close();
+	boolean isIdentifier(Scanner in) {
+		return in.hasNext(Pattern.compile("[a-zA-Z].*"));
 	}
 
-	public static void main(String[] args) {
-		new Main().run();
+	void run(String[] args) {
+		if (args.length == 0) {
+			System.err.println("ussage: Main [options] filename <filename>");
+		} else {
+			readInput(args);
+		}
+	}
+
+	void readInput(String[] args) {
+		try {
+			args = parseOptions(args);
+			Scanner[] files = openFiles(args);
+			for (Scanner in : files) {
+				parseText(in);
+			}
+		} catch (APException e) {
+			System.err.println(e.getMessage());
+		} catch (IndexOutOfBoundsException ie) {
+			System.err.println("ussage: Main [options] filename <filename>");
+		}
+	}
+
+	String[] parseOptions(String[] args) throws APException {
+		while (args[0].startsWith("-")) {
+			if (args[0].equals("-i")) {
+				lowercase = true;
+			} else if (args[0].equals("-d")) {
+				descending = true;
+			} else {
+				throw new APException("Illegal argument given: " + args[0]);
+			}
+			args = Arrays.copyOfRange(args, 1, args.length);
+		}
+		return args;
+	}
+
+	Scanner[] openFiles(String[] files) throws APException {
+		Scanner[] fileScanners = new Scanner[files.length];
+		for (int i = 0; i < fileScanners.length; i++) {
+			File f = new File(files[i]);
+			try {
+				fileScanners[i] = new Scanner(f);
+			} catch (FileNotFoundException e) {
+				throw new APException("The file: " + files[i]
+						+ " does not exist");
+			}
+		}
+
+		return fileScanners;
+	}
+
+	void parseText(Scanner in) {
+		in.useDelimiter(Pattern.compile("[^a-zA-Z0-9]+"));
+		while (in.hasNext()) {
+			if (isIdentifier(in)) {
+				System.out.println(in.next());
+			} else {
+				in.next(); //ignore the non-identifier
+			}
+		}
+	}
+	
+	void printOutput(){
+		//print the tree
+	}
+
+	public static void main(String[] args) {		
+		new Main().run(args);
 	}
 
 }
